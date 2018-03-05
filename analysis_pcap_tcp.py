726c600fd02df3a7d0d3e8642b6951228f02a1aa
Alfred Wu <awu123@gmail.com>
2018-03-04 22:41:33 -0500

    Flow matching working.

--- analysis_pcap_tcp.py
+++ analysis_pcap_tcp.py
@@ -3,17 +3,36 @@ import struct
 
 class Flow:
 
+    data_sent = 0
+    unacked_packets = {}
+
+    def __init__(self, sender_port, receiver_port):
+        self.sender_port = sender_port
+        self.receiver_port = receiver_port
+
     
+
+    def match(self, source_port, dest_port):
+        if self.sender_port == source_port or self.sender_port == dest_port:
+            return True
+        else:
+            return False
+
+
 
 
 def test():
     f = open('assignment2.pcap', 'rb')
     pcap =  dpkt.pcap.Reader(f)
 
+    flows = []
+
+    count = 0
+
     for ts, buf in pcap:
-        print(ts)
-        print(buf)
-        print(len(buf))
+        # print(ts)
+        # print(buf)
+        # print(len(buf))
 
         tcp_header = buf[34:54]
 
@@ -28,22 +47,39 @@ def test():
         flags = (flags << 2) >> 2
 
 
-        print(source_port)
-        print(dest_port)
-        print(seq_num)
-        print(ack_num)
-        print(flags)
+        # print(source_port)
+        # print(dest_port)
+        # print(seq_num)
+        # print(ack_num)
+        # print(flags)
 
         if flags == 2:
-            print("SYN")
-        elif flags == 16:
-            print("ACK")
-        elif flags == 17:
-            print("FIN ACK")
-        elif flags == 18:
-            print("SYN ACK")
-        elif flags == 24:
-            print("PSH ACK")
+            new_flow = Flow(sender_port=source_port, receiver_port=dest_port)
+            flows.append(new_flow)
+            current_flow = new_flow
+        else:
+            for flow in flows:
+                if flow.match(source_port=source_port, dest_port=dest_port):
+                    current_flow = flow
+                    break
+
+
+
+
+
+
+        count += 1
+        if count == 10:
+            break
+
+        # elif flags == 16:
+        #     print("ACK")
+        # elif flags == 17:
+        #     print("FIN ACK")
+        # elif flags == 18:
+        #     print("SYN ACK")
+        # elif flags == 24:
+        #     print("PSH ACK")
 
 
         # result = struct.unpack("14c52c", buf)
