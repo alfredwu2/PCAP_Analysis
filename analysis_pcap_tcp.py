9a73d48a32b1a1aedd1f48540b2a400409069de3
Alfred Wu <awu123@gmail.com>
2018-03-04 21:12:48 -0500

    Extracting flag bit information.

--- analysis_pcap_tcp.py
+++ analysis_pcap_tcp.py
@@ -16,18 +16,34 @@ def test():
 
         tcp_header = buf[34:54]
 
-        result = struct.unpack_from("!HHI", tcp_header)
+        result = struct.unpack_from("!HHIIBB", tcp_header)
 
         source_port = result[0]
         dest_port = result[1]
         seq_num = result[2]
+        ack_num = result[3]
+        unused = result[4]
+        flags = result[5]
+        flags = (flags << 2) >> 2
 
 
         print(source_port)
         print(dest_port)
         print(seq_num)
+        print(ack_num)
+        print(flags)
 
-        break
+        if flags == 2:
+            print("SYN")
+        elif flags == 16:
+            print("ACK")
+        elif flags == 17:
+            print("FIN ACK")
+        elif flags == 18:
+            print("SYN ACK")
+        elif flags == 24:
+            print("PSH ACK")
+
 
         # result = struct.unpack("14c52c", buf)
         # print(result)
